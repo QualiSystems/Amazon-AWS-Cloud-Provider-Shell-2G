a33346b47660364ed75ca1d9e92064f16322ebfc
Kyrylo Maksymenko <saklar13@gmail.com>
2021-01-05 19:48:55 +0200

    initial

---
+++ tests/test_aws-cloud-provider-shell-2g.py
@@ -0,0 +1,20 @@
+import unittest
+
+from src.driver import AwsCloudProviderShell2GDriver
+
+
+class TestAwsCloudProviderShell2GDriver(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_000_something(self):
+        pass
+
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(unittest.main())
